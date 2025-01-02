1109c7be55999e4192cb1d937610bff570f53b61
Julian Ehrenberg <j.ehrenberg@campus.lmu.de>
2025-01-02 23:46:16 +0100

    end 02/01

--- Counterfactuals.R
+++ Counterfactuals.R
@@ -1,5 +1,12 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+
 LendingAcc <- read.csv("C:/Users/jeroe/Uni_C/TrustML/LendingClub_2007_to_2018Q4.csv/accepted_2007_to_2018Q4.csv")
 head(LendingAcc)
 
 LendingRej <- read.csv("C:/Users/jeroe/Uni_C/TrustML/LendingClub_2007_to_2018Q4.csv/rejected_2007_to_2018Q4.csv")
+head(LendingRej)
+
 
