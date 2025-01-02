4cadb901b70655bc86c4705c6946909aaf1d3b52
Julian Ehrenberg <j.ehrenberg@campus.lmu.de>
2025-01-02 23:00:50 +0100

    Initial commit

---
+++ Counterfactuals.R
@@ -0,0 +1,5 @@
+LendingAcc <- read.csv("C:/Users/jeroe/Uni_C/TrustML/LendingClub_2007_to_2018Q4.csv/accepted_2007_to_2018Q4.csv")
+head(LendingAcc)
+
+LendingRej <- read.csv("C:/Users/jeroe/Uni_C/TrustML/LendingClub_2007_to_2018Q4.csv/rejected_2007_to_2018Q4.csv")
+
